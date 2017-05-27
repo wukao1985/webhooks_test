af57a465e0d08b8f15010e05811ad86ebd0d439e
Kao Wu <wukao@fb.com>
2017-05-26 17:32:50 -0700

    test

--- index.js
+++ index.js
@@ -37,10 +37,12 @@ app.post('/webhooks', function(req, res) {
     res.sendStatus(401);
     return;
   }
+
+  received_counts += req.body.entry.length;
+
   var i = 0;
   //console.log(JSON.stringify(req.body, null, 2));
   for (i = 0; i < req.body.entry.length; i++) {
-    received_counts += 1;
     //console.log(Math.round((new Date()).getTime() / 1000) - req.body.entry[i].changes.value.conversion_timestamp, received_counts);
     console.log(Math.round((new Date()).getTime() / 1000) - Date.parse(req.body.entry[i].changes[0].value.conversion_timestamp)/1000);
   }
